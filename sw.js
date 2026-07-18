11de016afc3df0e5e5d682e80e1b186de8f2c813
Igor Chede <igorccllc1@gmail.com>
2026-07-18 18:24:54 -0300

    update: 2026-07-18 18:24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'finplan-20260718182445';
+const CACHE = 'finplan-20260718182453';
 const PRECACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
